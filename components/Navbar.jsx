9ff5d833bc28d21b0f10634b13549a89ff9e172f
Prasham1710 <jainprasham17@gmail.com>
2023-04-01 02:20:46 +0530

    navbar and link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import Image from 'next/image'
+import NavLogo from '../public/assets/navLogo.png'
+import Link from 'next/link'
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <Image 
+            src={NavLogo}
+            alt="/" 
+            width='125'
+            height='50'
+        />
         <div>
-            hlo
+            <ul className=' hidden md:flex'>
+            <Link href='/'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+            Home</li>
+            </Link>
+            <Link href='/'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+            About</li>
+            </Link>
+            <Link href='/'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+            Skills</li>
+            </Link>
+            <Link href='/'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+            Project</li>
+            </Link>
+            <Link href='/'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+            Contact</li>
+            </Link>
+        </ul>
+        <div className='md:hidden'>
+            <AiOutlineMenu size = {25}/>
+        </div>
+        </div>
         </div>
     </div>
   )
