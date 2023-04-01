25080322ed3609d49fb7a68ddee39519c020b5b0
Prasham1710 <jainprasham17@gmail.com>
2023-04-02 04:02:30 +0530

    update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,10 +4,12 @@ import Todo from '../public/assets/projects/Todo.png'
 import Link from 'next/link'
 const Projects = () => {
   return (
-    <div className='w-full'>
-       <div className='max-w-[1240] mx-auto px-2 py-16'>
-        <p>Projects </p>
-        <h2 className='py-4'> What I've Built</h2>
+    <div id='projects' className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'> What  Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
             <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
             <Image className='rounded-xl group-hover:opacity-10' src={Todo} alt='/' /> 
