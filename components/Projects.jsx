bad1eb9ab6d320628c2eaf2a22fad88936e7ea71
Prasham1710 <jainprasham17@gmail.com>
2023-04-02 04:43:59 +0530

    projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import Image from 'next/image'
 import Todo from '../public/assets/projects/Todo.png'
-import Link from 'next/link'
+import Bmi from '../public/assets/projects/BMI.png'
+import Capture from '../public/assets/projects/Capture.png'
+import Chat from '../public/assets/projects/Chat_app.png'
+
+import ProjectItem from './ProjectItem'
 const Projects = () => {
   return (
     <div id='projects' className='w-full'>
@@ -11,16 +14,27 @@ const Projects = () => {
         </p>
         <h2 className='py-4'> What  Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-            <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={Todo} alt='/' /> 
-            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl text-white tracking-wider text-center'>Todo</h3>
-            <p className='pb-4 pt-2 text-white text-center'>Reactjs</p>
-            <Link href ='/'>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
-            </Link>
-            </div>
-            </div>
+        <ProjectItem t
+        title='Todo' 
+        background={Todo} 
+        projectUrl='https://todoapp-gules-sigma.vercel.app/' 
+        />
+        <ProjectItem t
+        title='Todo' 
+        background={Bmi} 
+        projectUrl='https://bmi-claculator.vercel.app/' 
+        />
+        <ProjectItem t
+        title='Todo' 
+        background={Capture} 
+        projectUrl='/todo' 
+        />
+        <ProjectItem t
+        title='Todo' 
+        background={Chat} 
+        projectUrl='/todo' 
+        />
+        
         </div>
         </div>
     </div>
