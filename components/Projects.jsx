d6c1aa0fbc1615031e38afc97204e96ef607c293
Prasham1710 <jainprasham17@gmail.com>
2023-04-02 03:14:38 +0530

    Projects

---
+++ components/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import Todo from '../public/assets/projects/Todo.png'
+const Projects = () => {
+  return (
+    <div className='w-full'>
+       <div className='max-w-[1240] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects </p>
+        <h2 className='py-4'> What I've Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+            <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+            <Image className='rounded-xl group-hover:opacity-10' src={Todo} alt='/' /> 
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import About from '@/components/About'
 import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
+import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
 import Head from 'next/head'
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <Main/>
       <About/>
       <Skills />
+      <Projects />
     </>
   )
 }
