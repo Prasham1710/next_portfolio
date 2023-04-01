e67471808025e10955f1c1fcaf0580da1645035d
Prasham1710 <jainprasham17@gmail.com>
2023-04-02 05:23:30 +0530

    protects done

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-const ProjectItem = ({title,backgroundImg, projectUrl}) => {
+const ProjectlItem = ({title,backgroundImg, projectUrl,tech}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
-        <p className='pb-4 pt-2 text-white text-center'>Reactjs</p>
-        <Link href ={projectUrl}>
-                <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+        <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
+        <Link href={projectUrl}>
+            <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>Code</p>
         </Link>
-        </div>
+        <Link href={projectUrl}>
+            <p className='mt-2 text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>Live</p>
+        </Link>
     </div>
+ </div>
   )
 }
-
-export default ProjectItem
+export default ProjectlItem
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 import Todo from '../public/assets/projects/Todo.png'
 import Bmi from '../public/assets/projects/BMI.png'
 import Capture from '../public/assets/projects/Capture.png'
 import Chat from '../public/assets/projects/Chat_app.png'
+import Password from '../public/assets/projects/password.png'
+import Travel from '../public/assets/projects/Travel_website.png'
+import Data from '../public/assets/projects/data_pack.png'
+import ProjectlItem from './ProjectItem'
 
-import ProjectItem from './ProjectItem'
 const Projects = () => {
   return (
     <div id='projects' className='w-full'>
@@ -14,31 +19,61 @@ const Projects = () => {
         </p>
         <h2 className='py-4'> What  Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-        <ProjectItem t
-        title='Todo' 
-        background={Todo} 
-        projectUrl='https://todoapp-gules-sigma.vercel.app/' 
-        />
-        <ProjectItem t
-        title='Todo' 
-        background={Bmi} 
-        projectUrl='https://bmi-claculator.vercel.app/' 
-        />
-        <ProjectItem t
-        title='Todo' 
-        background={Capture} 
-        projectUrl='/todo' 
-        />
-        <ProjectItem t
-        title='Todo' 
-        background={Chat} 
-        projectUrl='/todo' 
-        />
-        
+          <ProjectlItem
+            title='Todo app'
+            backgroundImg={Todo}
+            projectUrl='https://todoapp-gules-sigma.vercel.app/'
+            tech='React JS'
+          />
+          <ProjectlItem
+            title='Capture App'
+            backgroundImg={Capture}
+            projectUrl='/crypto'
+            tech='React JS'
+
+          />
+          <ProjectlItem
+            title='Password Generator'
+            backgroundImg={Password}
+            projectUrl='https://password-generator-beta-black.vercel.app/'
+            tech='React JS'
+
+          />
+          <ProjectlItem
+            title='BMI calculator'
+            backgroundImg={Bmi}
+            projectUrl='https://bmi-claculator.vercel.app/'
+            tech='React JS'
+
+          />
+          <ProjectlItem
+            title='Data Pack '
+            backgroundImg={Data}
+            projectUrl='https://travel-website-pearl-six.vercel.app/'
+            tech='React JS'
+
+          
+          />
+          <ProjectlItem
+            title='Travel Website'
+            backgroundImg={Travel}
+            projectUrl='https://travel-website-pearl-six.vercel.app/'
+            tech='React JS'
+          />
+          <ProjectlItem
+            title='Chat app'
+            backgroundImg={Chat}
+            projectUrl='/'
+            tech='React JS'
+
+          />
+          
+           
+          
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects
