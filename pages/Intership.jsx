d9ec754baf7bc19658d05cc86fa08425caebabb6
Prasham1710 <jainprasham17@gmail.com>
2023-04-04 00:23:15 +0530

    deploy

--- pages/Intership.jsx
+++ pages/Intership.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Intern from '../public/assets/projects/internship.png'
 import Link from 'next/link';
-const Internship = () => {
+const Intership = () => {
   return (
     <div className='w-full'>
       <div className='w-screen h-[50vh] relative'>
@@ -56,4 +56,4 @@ const Internship = () => {
   )
 }
 
-export default Internship
+export default Intership
