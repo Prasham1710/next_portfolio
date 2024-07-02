17a60b5b7f8fcd4a2bdae11bb7da9fc7dcb47bd4
Prasham1710 <jainprasham17@gmail.com>
2024-07-02 13:37:06 +0530

    deployment

--- pages/Hos.jsx
+++ pages/Hos.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { RiRadioButtonFill } from 'react-icons/ri';
-import Hos from '../public/assets/projects/Hos.JPG'
+import Hos from '../public/assets/projects/hos.JPG'
 import Link from 'next/link';
 const Hospital = () => {
   return (
